919a302c1cc24017171647091f0bba803c18e7ff
Bafana Promise <bafana.promise@neutrinos.com>
2026-07-11 15:32:09 +0200

    add application layout with navbar, router outlet, and footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './core/navbar/navbar';
+import { Footer } from './core/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
