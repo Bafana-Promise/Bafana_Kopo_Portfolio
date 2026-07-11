5f107e091dd4e2a4e496b53c398c41f456ef3141
Bafana Promise <bafana.promise@neutrinos.com>
2026-07-11 13:36:21 +0200

    Implemented UI for Home Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Projects } from './pages/projects/projects';
+import { Experience } from './pages/experience/experience';
+import { Testimonials } from './pages/testimonials/testimonials';
+import { Contact } from './pages/contact/contact';
+import { Mentorship } from './pages/mentorship/mentorship';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'projects', component: Projects },
+  { path: 'experience', component: Experience },
+  { path: 'testimonials', component: Testimonials },
+  { path: 'contact', component: Contact },
+  { path: 'mentorship', component: Mentorship }
+];
