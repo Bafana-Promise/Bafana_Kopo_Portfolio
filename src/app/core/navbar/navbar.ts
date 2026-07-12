5d68b82ffd4c5cfaadcfe9f12ca0920dc79fd17c
Bafana Promise <bafana.promise@neutrinos.com>
2026-07-12 13:11:50 +0200

    Installed Angular material

--- src/app/core/navbar/navbar.ts
+++ src/app/core/navbar/navbar.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatIconModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
