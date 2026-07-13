2244086a5c874062ee7c9fcd5271cb317120d3d2
Bafana Promise <bafana.promise@neutrinos.com>
2026-07-13 08:54:02 +0200

    Added project section and skills section

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    CommonModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home { }
+export class Home {
+
+  skillSections = [
+    {
+      title: 'Web Design',
+      skills: [
+        'UI/UX Design',
+        'Responsive Design',
+        'Wireframing',
+        'Figma'
+      ]
+    },
+    {
+      title: 'Frontend',
+      skills: [
+        'HTML 5',
+        'CSS3',
+        'Javascript',
+        'Angular',
+        'React'
+      ]
+    },
+    {
+      title: 'Backend',
+      skills: [
+        'NodeJs',
+        'Node-Red',
+        'ExpressJS',
+        'MongoDB',
+        'PostgreSQL'
+      ]
+    }
+  ];
+
+}
